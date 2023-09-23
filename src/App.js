5ccbea04adc0fe864148ddde99aa0177a2572713
Lancelot <bulanlancelot@gmail.com>
2023-09-24 02:40:46 +0800

    css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
-function App() {
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c16883c6";
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search)
+  }
+
+  useEffect(() =>{
+    searchMovies('Spiderman');
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
-}
+};
 
 export default App;
