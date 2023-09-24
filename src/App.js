edb25a450e8b7fcda096acd361d2cba8d0117558
Lancelot <bulanlancelot@gmail.com>
2023-09-25 02:00:47 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
         />
       </div>
 
-      {movies?.length > 0 ? (
+      {movies.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
